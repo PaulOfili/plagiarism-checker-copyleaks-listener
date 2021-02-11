eb898f9c08fdfb8235ef181618e9b3116860fb22
Paul Ofili <paulofili42@gmail.com>
2021-02-11 05:40:23 +0100

    Initial commits

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+
+app.post("/webhook/:status/:scanId", (req, res) => {
+    const scanId = req.params.scanId
+    const status = req.params.status
+    const result = req.body
+
+    const payload = {...result, scanId, status}
+    io.emit("copyleaks", payload);
+    res.send({response: "Received successfully"}).status(200);
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
